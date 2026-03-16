e5f8578ca09d9f24b93d5b92fda82d685e1cf42d
Tomas093 <tjmonteiro@mail.austral.edu.ar>
2026-03-16 11:31:11 -0300

    refactor: rename BlockChain to Blockchain, restructure transaction handling, and introduce TransactionPool and Chain classes

--- api/src/main/kotlin/entities/transaction/rules/PositiveAmountRule.kt
+++ api/src/main/kotlin/entities/transaction/rules/PositiveAmountRule.kt
@@ -3,7 +3,7 @@ package api.entities.transaction.rules
 import api.dtos.Transaction
 
 class PositiveAmountRule : TransactionRule {
-    override fun isValid(transaction: Transaction): Boolean = transaction.amount > 0.0f
+    override fun isValid(transaction: Transaction): Boolean = transaction.amount > 0L
 
     override fun getErrorMessage() = "The amount of transactions must be positive"
 }
